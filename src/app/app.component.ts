eb17463995e2cf50f8b018d28bf1c5c03e073bb3
teodor <teo@privacysystems.eu>
2020-12-28 11:40:49 -0800

    Add demo example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'isi';
+  title = 'isi'
+  // Set our map properties
+  mapCenter = [-122.4194, 37.7749];
+  basemapType = 'satellite';
+  mapZoomLevel = 12;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
 }
